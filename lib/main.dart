12ab1554c81122b8ac26173be6fb3f098976b83c
YevheniiLutai <53271797+YevheniiLutai@users.noreply.github.com>
2020-09-23 16:39:03 +0300

    Add box with image, text, title. Work with classes and other thing in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -14,24 +14,98 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My first Flutter app Lutai Yevhenii TI-71'),
+          title: Text('First demo Flutter app by Lutai Yevhenii TI-71'),
         ),
-        body:
-        Center(
-          child: Container(
-            color: Colors.blueGrey,
-            child: Transform(
-              alignment: Alignment.topRight,
-              transform: Matrix4.skewY(0.3)..rotateZ(-3.14 / 12.0),
-              child: Container(
-                padding: const EdgeInsets.all(12.0),
-                color: const Color(0xFFE8581C),
-                child: const Text('Hello, I am Lutai Yevhenii TI-71'),
-              ),
-            ),
-          )
-        ),
+        body: NewsBox('Lutai Yevhenii TI-71', '''Hello, it is my first Flutter app.
+         by Lutai Yevhenii TI-71''',
+          imageurl: 'image/50-05.jpg', num: 1),
       ),
     );
   }
 }
+ class NewsBoxLike extends StatefulWidget {
+  final int _num;
+  final bool _like;
+
+  NewsBoxLike(this._num, this._like);
+
+  @override
+   createState() => new NewsBoxLikeState(_num, _like);
+}
+
+class NewsBoxLikeState extends State<NewsBoxLike> {
+  int num;
+  bool like;
+
+  NewsBoxLikeState(this.num, this.like);
+
+  void pressButton(){
+    setState(() {
+      like = false;
+
+      if(like = true) num++;
+      else num--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   return Column(
+     children: [
+       Text('Like $num', textAlign: TextAlign.center),
+       IconButton(
+         icon: Icon(like ? Icons.favorite : Icons.favorite_border, size: 35.0, color:
+         Colors.deepOrange),
+         onPressed: pressButton,
+       )
+     ],
+   );
+  }
+}
+
+class NewsBox extends StatelessWidget{
+  final String _title;
+  final String _text;
+  String _imageurl;
+  int _num;
+  bool _like;
+
+  NewsBox(this._title, this._text, {String imageurl, int num = 0, bool like = false})
+  {
+    _imageurl = imageurl;
+    _num = num;
+    _like = like;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   if (_imageurl != null && _imageurl != '') return Container(
+     color:  Colors.blueGrey,
+     height: 150.0,
+     child: Row(children: [
+       Image.asset(_imageurl, width: 120.0, height: 140.0, fit: BoxFit.cover),
+       Expanded(child: Container(padding: EdgeInsets.all(8.0), child: Column(children: [
+         Text(_title,  style: TextStyle(fontSize: 26.0), overflow: TextOverflow.ellipsis),
+         Expanded(child: Text(_text, softWrap: true,  style: TextStyle(fontSize: 17.0), textAlign: TextAlign.justify,))
+       ]
+       ))
+       ), NewsBoxLike(_num, _like)
+     ],),
+   );
+   else return Container(
+       color: Colors.amber,
+       height: 150.0,
+       child:  Row(children: [
+         Expanded(child: Container(padding: EdgeInsets.all(5.0), child: Column(children: [
+           Text(_title, style: TextStyle(fontSize: 23.0),overflow: TextOverflow.ellipsis),
+           Expanded(child: Text(_text, softWrap: true, textAlign: TextAlign.justify,))
+         ]
+         ))
+         ),  NewsBoxLike(_num, _like)
+       ])
+   );
+
+  }
+}
+
+
